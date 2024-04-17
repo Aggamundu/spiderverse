14d67c4d3713d996cdcafea76a02d1b4c25ac029
Ryan Cepeda <ryancepeda2@gmail.com>
2024-04-17 11:21:41 -0400

    Add Data and DimensionNode classes

---
+++ Spiderverse/src/spiderman/Data.java
@@ -0,0 +1,27 @@
+package spiderman;
+
+public class Data {
+  private int number;
+private int canon;
+private int weight;
+
+public void setData (int number, int canon, int weight){
+this.number = number;
+this.canon = canon;
+this.weight = weight;
+}
+
+public int getNumber(){
+return number;
+}
+public int getCanon(){
+return canon;
+}
+public int getWeight(){
+  return weight;
+}
+
+}
+
+
+
---
+++ Spiderverse/src/spiderman/DimensionNode.java
@@ -0,0 +1,24 @@
+package spiderman;
+
+public class DimensionNode {
+  private Data data;
+private DimensionNode next;
+
+public DimensionNode (Data data, DimensionNode next){
+this.data = data;
+this.next = next;
+}
+public Data getData() {
+return data;
+}
+public void setData(Data data){
+this.data = data;
+}
+public DimensionNode getNextDimensionNode() {
+return next;
+}
+public void setDimensionNode (DimensionNode next){
+this.next = next;
+}
+
+}
